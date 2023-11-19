52f315ff54f94a006423e0246ebe00857e9a5080
Igor-apps <151463292+Igor-apps@users.noreply.github.com>
2023-11-19 19:58:39 +0100

    Update index.html

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
+<!DOCTYPE html>
 <html>
-<head>
-<title>Форма заявки с сайта</title>
-<script src="https://www.google.com/recaptcha/api.js" async defer></script>
-</head>
 <body>
-<form action="send.php" method="post">
-<input placeholder="Ваше имя" type="text" name="fio" required>
-<input placeholder="Ваш e-mail" type="email" name="email">
-<textarea name="message" cols="50" rows="10" placeholder="Напишите ваше сообщение сюда..."></textarea>
-<div class="g-recaptcha" data-sitekey="ключ сайта"></div>
-<input type="submit" value="Отправить">
-</form> 
+<h1>Hello World</h1>
+<p>I'm hosted with GitHub Pages.</p>
 </body>
 </html>
